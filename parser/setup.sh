693c37c4867f4599bfe80371bde2e51e878ec7df
Raphael Hoffmann <raphael.hoffmann@gmail.com>
2015-06-04 08:40:28 +0000

    add supervisor configuration to services
    Summary:
    upgrade to java8 and newer sbt-launch.jar
    
    add apt-get for supervisor
    
    fix logs dir error
    
    add gitignore
    
    Test Plan: manual
    
    Reviewers: feng
    
    Reviewed By: feng
    
    Differential Revision: https://phab.clearcutcorp.com/D24

--- parser/setup.sh
+++ parser/setup.sh
@@ -12,6 +12,17 @@ else
     echo "Skipping download: $DESTDIR/$FILENAME already exists"
 fi
 
+# java
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install -y sudo apt-get install openjdk-8-jdk
+
 # build parser
 cd $DIRNAME
 sbt/sbt stage
+
+# supervisor
+mkdir /opt/services/parser/logs
+sudo apt-get install -y supervisor
+sudo ln -s /opt/services/parser/conf/supervisor.conf /etc/supervisor/conf.d/parser.conf
+sudo service supervisor restart
