454cc4360625b043156147d700976871de8c3fbb
Jaeho Shin <netj@sparcs.org>
2015-07-18 01:45:01 -0700

    Improves View's setup, run, env scripts
    - to avoid uses of sudo
    - to be able to run it on environments other than Mac with iTerm
    - gitignore

--- view/run.sh
+++ view/run.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+if [[ $(uname) = Darwin ]] &&
+    osascript -e 'get path to application "iTerm"' &>/dev/null; then
+    # On a Mac with iTerm.app, do something nice
+    start() { ( source ./util/tab && tab "$@" ); }
+else
+    # otherwise, just run the process
+    start() { local title=$1; shift; "$@" & }
+    trap wait EXIT
+fi
+
 # launch elasticsearch
-sudo bash -c "source ./util/tab && tab ElasticSearch util/elasticsearch-1.6.0/bin/elasticsearch "
+
+start "ElasticSearch" elasticsearch
 
 # launch nodejs
-sudo bash -c "source ./util/tab && tab Nodejs npm start "
+start "Nodejs" npm start
 
 # launch react jsx watch
-sudo bash -c "source ./util/tab && tab Reactjs jsx --watch view/ public/js"
+start "Reactjs" jsx --watch view/ public/js
 
