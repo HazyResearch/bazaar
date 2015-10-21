71b46a865c4a1581b0b63cd17679b5a9f31f0055
Raphael Hoffmann <raphael.hoffmann@gmail.com>
2015-10-20 18:45:14 -0700

    Fixes naming of fields in json output

--- pipe/src/main/scala/com/clearcut/pipe/io/JSONWriter.scala
+++ pipe/src/main/scala/com/clearcut/pipe/io/JSONWriter.scala
@@ -1,6 +1,7 @@
 package com.clearcut.pipe.io
 
 import java.io.{OutputStreamWriter, FileOutputStream, BufferedWriter}
+import com.clearcut.pipe.model._
 
 import com.clearcut.pipe.Schema
 import org.json4s._
@@ -12,11 +13,14 @@ class JsonWriter(out:String) extends Writer {
   implicit val formats = DefaultFormats
 
   val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(out)))
+  var names:Seq[String] = null
 
-  def setSchema(schema:Schema): Unit = {}
+  def setSchema(schema:Schema): Unit = {
+    names = schema.annTyps.map(t => lowerFirst(t))
+  }
 
   def write(annotations:Seq[AnyRef]) = {
-    val arr:Seq[JObject] = annotations.map(x => JObject(JField(x.getClass.getName, Extraction.decompose(x))))
+    val arr:Seq[JObject] = annotations.zip(names).map { case (x,n) => JObject(JField(n, Extraction.decompose(x)))}
     var o:JObject = arr(0)
     for (i <- 1 until arr.length)
       o = o merge arr(i)
